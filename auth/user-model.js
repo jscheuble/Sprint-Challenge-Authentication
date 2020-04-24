717c12c8755a28218dcf31e6fdcf174cb4556d9e
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-04-24 08:42:48 -0700

    login endpoint works & createToken successful

--- auth/user-model.js
+++ auth/user-model.js
@@ -1,13 +1,20 @@
 const db = require("../database/dbConfig");
 
 module.exports = {
+  get,
   add,
   getById,
   getBy,
 };
 
+function get() {
+  return db("users");
+}
+
 async function add(user) {
-  return db("users").insert(user);
+  return db("users")
+    .insert(user)
+    .then((id) => getById(id[0]));
 }
 
 function getById(id) {
